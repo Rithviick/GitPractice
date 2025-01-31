1bdb588d5fe72b5cbda34b474604c26382b8f159
rithviick <rithvickreddy.k@gmail.com>
2025-01-31 11:59:47 +0530

    Hello World Program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,7 @@
 package GitPractice;
 
-class HelloWorld
-{
-    public static void main(String[] args)
-    {
-        System.out.println("My First Java Program.");
+class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
     }
 }
